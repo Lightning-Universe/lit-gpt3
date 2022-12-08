2b4f265988797dd2382e6449ba4f5f9e5f45b235
DaniDapena <50242217+DaniDapena@users.noreply.github.com>
2022-12-08 12:17:26 -0700

    play-response

--- examples/whisper/client.py
+++ examples/whisper/client.py
@@ -1,10 +1,19 @@
-import requests, base64
+import requests, base64, os, gtts
+
+
+def create_mp3(text):
+    myobj = gtts.gTTS(text=text, lang="en", slow=False)
+    myobj.save("reponse.mp3")
+    # Playing the converted file
+
 
 f = open("dani.mp3", "rb")
 waveform = base64.b64encode(f.read()).decode("utf-8")
 f.close()
 
 response = requests.post(
-    "https://dslma-01gkqgdn2gzv9fm1v43p1dxer8.litng-ai-03.litng.ai/predict", json={"text": waveform}
+    "https://uodfm-01gkshzer9kt7mnedd7vqps84c.litng-ai-03.litng.ai/predict", json={"text": waveform}
 )
+
 print(response.json()["text"])
+create_mp3(response.json()["text"])
