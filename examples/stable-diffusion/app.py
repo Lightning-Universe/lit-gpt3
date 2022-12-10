4da71f3dfd72a2ae895adb4f6424db0001b684d4
DaniDapena <50242217+DaniDapena@users.noreply.github.com>
2022-12-09 17:11:52 -0700

    Co-authored-by: Alec Merdler <alecmerdler@users.noreply.github.com>

--- examples/stable-diffusion/app.py
+++ examples/stable-diffusion/app.py
@@ -1,11 +1,16 @@
-# !pip install 'git+https://github.com/Lightning-AI/lightning-diffusion-component.git'
 # !pip install 'git+https://github.com/Lightning-AI/lightning-gpt3.git'
+# !pip install 'git+https://github.com/Lightning-AI/LAI-API-Access-UI-Component.git@diffusion'
+# !curl https://raw.githubusercontent.com/Lightning-AI/stablediffusion/main/configs/stable-diffusion/v2-inference-v.yaml -o v2-inference-v.yaml
+
 
 import lightning as L
-import torch, os, io, base64, diffusers, pydantic
+import torch, os, io, base64, pydantic
 from lightning.app.components import Image, serve
-from lightning_diffusion import download_from_lightning_cloud
+from ldm.lightning import LightningStableDiffusion, PromptDataset
 from lightning_gpt3 import LightningGPT3
+
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 
 class Text(pydantic.BaseModel):
@@ -21,15 +26,37 @@ class StableDiffusionServer(serve.PythonServer):
         self._gpt3 = LightningGPT3(api_key=os.getenv("OPENAI_API_KEY"))
 
     def setup(self):
-        download_from_lightning_cloud("daniela/stable_diffusion", version="latest", output_dir="model")
-        self._model = diffusers.StableDiffusionPipeline.from_pretrained("model").to(
-            "cuda" if torch.cuda.is_available() else "cpu"
+        os.system(
+            "curl -C - https://pl-public-data.s3.amazonaws.com/dream_stable_diffusion/768-v-ema.ckpt -o 768-v-ema.ckpt"
+        )
+        os.system("echo *.ckpt > .lightningignore ")
+
+        self._trainer = L.Trainer(
+            accelerator="auto",
+            devices=1,
+            precision=16 if torch.cuda.is_available() else 32,
+            enable_progress_bar=False,
+            inference_mode=torch.cuda.is_available(),
         )
 
+        self._model = LightningStableDiffusion(
+            config_path="v2-inference-v.yaml",
+            checkpoint_path="768-v-ema.ckpt",
+            device=self._trainer.strategy.root_device.type,
+            size=768,
+        )
+
+        if torch.cuda.is_available():
+            self._model = self._model.to(torch.float16)
+            torch.cuda.empty_cache()
+
     def predict(self, request: Text):
+        print(self._trainer.strategy.root_device.type)
         prompt = "Describe a " + request.text + " picture"
         enhanced_prompt = self._gpt3.generate(prompt=prompt, max_tokens=40)[2::]
-        image = self._model(enhanced_prompt)[0][0]
+        image = self._trainer.predict(self._model, torch.utils.data.DataLoader(PromptDataset([enhanced_prompt])),)[
+            0
+        ][0]
         buffer = io.BytesIO()
         image.save(buffer, format="PNG")
         img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
--- examples/stable-diffusion/client.py
+++ examples/stable-diffusion/client.py
@@ -2,8 +2,8 @@ import base64, io, requests, PIL.Image as Image
 
 if __name__ == "__main__":
     response = requests.post(
-        "http://127.0.0.1:7777/predict",
-        json={"text": "fairy tail-inspired bedroom"},
+        "http://127.0.0.1:50610/predict",
+        json={"text": "forest-inspired bedroom"},
     )
 
     image = Image.open(io.BytesIO(base64.b64decode(response.json()["image"][22:])))
